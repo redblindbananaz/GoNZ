145c6fe5e39473912764af6a55048a52aaa78681
redblindbananaz <sebhilltop@gmail.com>
2023-05-22 15:57:23 +1200

    creation of agent and tour views

--- agent/views.py
+++ agent/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Tour
+from django.views.generic import ListView, DetailView
+from .models import Agent
 
 # Create your views here.
 
-class TourListView(ListView):
-    model = Tour
-    template_name = 'tour_list.html'
-    context_object_name = 'tour_list'
-    paginate_by = 10
+class AgentListView(ListView):
+    model = Agent
+    template_name = 'agent_list.html'
+    context_object_name = 'agents'
+
+class AgentDetailView(DetailView):
+    model = Agent
+    template_name = 'agent_detail.html'
+    context_object_name = 'agent'
--- tour/views.py
+++ tour/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Tour
 
 # Create your views here.
+
+class TourListView(ListView):
+    model = Tour
+    template_name = 'tour_list.html'
+    context_object_name = 'tours'
+
+class TourDetailView(DetailView):
+    model = Tour
+    template_name = 'tour_detail.html'
+    context_object_name = 'tour'
