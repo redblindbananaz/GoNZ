be62a5311d9004b8ce7026bbe51b0c073532af24
redblindbananaz <sebhilltop@gmail.com>
2023-05-24 16:34:57 +1200

    groups,users in admin panel

--- agent/models.py
+++ agent/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
 class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
+    department = models.CharField(max_length=100, default='Agent')
+    position = models.CharField(max_length=100, default='Agent')
     name = models.CharField(max_length=100)
     email = models.EmailField()
     phone = models.CharField(max_length=20)
