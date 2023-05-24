c4ca2d1829c23214f4e7068bb62c672599f84240
redblindbananaz <sebhilltop@gmail.com>
2023-05-24 18:54:41 +1200

    media set up and for loops for tours

--- GoNZ/settings.py
+++ GoNZ/settings.py
@@ -73,8 +73,9 @@ TEMPLATES = [
     },
 ]
 
-MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 WSGI_APPLICATION = "GoNZ.wsgi.application"
--- tour/views.py
+++ tour/views.py
@@ -8,6 +8,8 @@ class TourListView(ListView):
     model = Tour
     template_name = 'tour/tour_list.html'
     context_object_name = 'tours'
+    paginate_by = 3
+    ordering = 'id'  # Specify the ordering field for consistent pagination
 
 class TourDetailView(DetailView):
     model = Tour
