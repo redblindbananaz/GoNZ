91ead3e87775281ebdc409e78b6f235f5beaa7d4
redblindbananaz <sebhilltop@gmail.com>
2023-05-24 23:47:27 +1200

    trying pagination

--- tour/views.py
+++ tour/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.core.paginator import Paginator
 from .models import Tour
 
 # Create your views here.
@@ -9,7 +10,13 @@ class TourListView(ListView):
     template_name = 'tour/tour_list.html'
     context_object_name = 'tours'
     paginate_by = 3
-    ordering = 'id'  # Specify the ordering field for consistent pagination
+    ordering = ['-id']  # Specify the ordering field for consistent pagination
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        paginator = Paginator(queryset, self.paginate_by)
+        page_number = self.request.GET.get('page')
+        return paginator.get_page(page_number)
 
 class TourDetailView(DetailView):
     model = Tour
