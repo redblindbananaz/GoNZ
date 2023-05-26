381f8235bc2ad6572c0200341b2900775f3704e8
redblindbananaz <sebhilltop@gmail.com>
2023-05-26 20:11:27 +1200

    pagination and styling

--- tour/views.py
+++ tour/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.core.paginator import Paginator
+from django.http import request
+
 from .models import Tour
 
 # Create your views here.
@@ -11,12 +13,6 @@ class TourListView(ListView):
     context_object_name = 'tours'
     paginate_by = 3
     ordering = ['-id']  # Specify the ordering field for consistent pagination, in this case the last one first.
-    
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        paginator = Paginator(queryset, self.paginate_by)
-        page_number = self.request.GET.get('page')
-        return paginator.get_page(page_number)
 
 class TourDetailView(DetailView):
     model = Tour
