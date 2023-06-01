3847f34760a36ea6acdb00b52a44174efd3a3eb7
redblindbananaz <sebhilltop@gmail.com>
2023-06-02 11:21:03 +1200

    agent details tour filter

--- agent/views.py
+++ agent/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Agent
+from tour.models import Tour
 from django.contrib.auth.models import Group
 
 # Create your views here.
@@ -16,3 +17,8 @@ class AgentDetailView(DetailView):
     model = Agent
     template_name = 'agent/agent_detail.html'
     context_object_name = 'agent'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tours'] = Tour.objects.filter(agent=self.object)
+        return context
